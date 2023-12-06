260eeca7a7aaa85687ca89165654bb63b70dd305
Hojin Jang <jhojin7@gmail.com>
2023-12-06 16:12:01 +0900

    mysql+adminer

---
+++ README.md
@@ -0,0 +1,2 @@
+# docker-stuff
+My list of docker images and compose files that I've use from time to time.
---
+++ mysql/init.sql
@@ -0,0 +1,10 @@
+create table tbl (
+	a int,
+	b int,
+	c varchar(100)
+);
+
+insert into tbl values
+	(1,2,"asdf"),
+	(2,99999,"asdf"),
+	(3,19999,"asdf");
